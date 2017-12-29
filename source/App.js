91662642827069e8aecd28e31423359b56041abb
Filipe Natanael <filipenatanael@live.com>
2017-12-29 19:11:00 -0200

    Fixed combineReducers
    Fixed combineReducers

--- source/App.js
+++ source/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
-import { createStone } from 'redux';
+import { createStore } from 'redux';
 
 import Routes from './Routes';
+import reducers from './reducers/index';
 
 export default class App extends Component<{}> {
   render() {
     return (
-      <Provider stone={createStone(reducers)}>
+      <Provider store={createStore(reducers)}>
         <Routes />
       </Provider>
     );
