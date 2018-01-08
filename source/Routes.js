48c942816975288f6df4ee62acda042c993e0cbd
Filipe Natanael <filipenatanael@live.com>
2018-01-08 19:02:19 -0200

    Refactoring loading component to ActivityIndicator
    Refactoring loading component to ActivityIndicator

--- source/Routes.js
+++ source/Routes.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-import { View, Text, AsyncStorage } from 'react-native';
+import { View, AsyncStorage, StyleSheet, ActivityIndicator } from 'react-native';
 import { Router, Scene } from 'react-native-router-flux';
 
 import LoginScreen from './components/LoginScreen';
 import SignUpScreen from './components/SignUpScreen';
 import MainScreen from './components/MainScreen';
 import WelcomeScreen from './components/WelcomeScreen';
+
+
+const loadingCount = true;
 
 export default class Routes extends Component {
   constructor(props) {
@@ -26,9 +29,20 @@ export default class Routes extends Component {
       }
     });
   }
+
+  renderAcessButton() {
+    return (
+      <ActivityIndicator size="large" color="#00ff00" />
+    )
+  }
+
   render() {
     if (this.state.loading) {
-      return <View><Text>Loading...</Text></View>;
+      return (
+        <View style={[styles.container, styles.horizontal]}>
+        { this.renderAcessButton() }
+        </View>
+      );
     }
     return (
       <Router navigationBarStyle={{ backgroundColor: '#115E54' }} titleStyle={{ color: 'white' }}>
@@ -42,3 +56,15 @@ export default class Routes extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+  horizontal: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10
+  }
+})
