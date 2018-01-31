034ff880787ec6a33ccfd0ae31ed223f9b6e3c6c
Filipe Natanael <filipenatanael@live.com>
2018-01-31 00:42:10 -0200

    Treating the flow of success
    Treating the flow of success

--- source/reducers/AppReducer.js
+++ source/reducers/AppReducer.js
@@ -1,16 +1,17 @@
 import * as types from '../actions/Types';
 
 const INITIAL_STATE = {
-  emailContact: 'Testing@example.com',
-  addContactError: ''
+  email_contact: 'Testing@example.com',
+  add_contact_error: '',
+  add_contact_success: false,
 };
 
 export default (state = INITIAL_STATE, action) => {
    switch (action.type) {
      case types.ADD_CONTACT:
-     return { ...state, emailContact: action.payload }
+     return { ...state, email_contact: action.payload }
      case types.ADD_CONTACT_ERROR:
-     return { ...state, addContactError: action.payload }
+     return { ...state, add_contact_error: action.payload }
      default:
      return state;
    }
