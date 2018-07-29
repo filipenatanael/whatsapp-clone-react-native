cd394d36cec42036c8e84b7e51c9a2ed018184f8
Filipe Natanael <filipenatanael@live.com>
2018-07-28 23:30:42 -0300

    Listing Contacts and Updating the DataSource

--- source/components/CallScane.js
+++ source/components/CallScane.js
@@ -1,7 +1,7 @@
 import firebase from 'firebase';
 import React, { Component } from 'react';
-import _ from 'lodash';
 import base64 from 'base-64';
+import _ from 'lodash';
 import { View, Text, ListView } from 'react-native';
 
 import { connect } from 'react-redux';
@@ -9,33 +9,38 @@ import { fetchContacts } from '../actions/AppActions'
 
 class CallScane extends Component {
 
-  constructor(props){
-    super(props);
-    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
-
-    this.state = {
-      dataSourceRef: ds.cloneWithRows([
-      'Registry 1',
-      'Registry 2',
-      'Registry 3',
-      'Registry 4',
-      'Registry 5'
-    ])
-  }
-  }
-
   componentWillMount() {
     this.props.fetchContacts(base64.encode(this.props.email_logged_in));
+    this.createDataSource(this.props.contacts);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps.contacts);
+  }
+
+  createDataSource(contacts) {
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 })
+    this.dataSource = ds.cloneWithRows(contacts)
+    // CallScane.prototype.dataSource (example)
   }
 
   render() {
     return (
       <ListView
-        dataSource={this.state.dataSourceRef}
-        renderRow={data => <View><Text>{ data }</Text></View>}
-      />
-    );
-  }
+      enableEmptySections
+      dataSource={this.dataSource}
+      renderRow={data => {
+        console.log(data);
+        return (<View>
+          <Text>{ data.name }</Text>
+          <Text>{ data.email }</Text>
+          </View>
+        )
+      }
+    }
+    />
+  );
+}
 }
 
 mapStateToProps = state => {
@@ -45,6 +50,7 @@ mapStateToProps = state => {
 
   return {
     email_logged_in: state.AppReducer.email_logged_in,
+    contacts: contacts
   }
 }
 
