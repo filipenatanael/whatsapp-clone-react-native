0eeacff1d4eb538675ca664fdfc705b22b583c79
Filipe Natanael <filipenatanael@live.com>
2018-07-29 23:27:31 -0300

    Fixed on underlineColorAndroid

--- source/components/Chat.js
+++ source/components/Chat.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 
 export default class Chat extends Component {
   render(){
     return (
-      <View>
-        <Text>Chat</Text>
+      <View style={{ flex: 1, marginTop: 50, backgroundColor: '#eee4dc', padding: 10 }}>
+
+      <View style={{ flex: 1, paddingBotton: 20  }}></View>
+
+      <View style={{ flexDirectio: 'row', height: 60 }}>
+        <TextInput
+          underlineColorAndroid='transparent'
+          style={{ flex: 4, backgroundColor: '#fff', fontSize: 15, borderRadius: 30 }}
+        />
+      </View>
+
       </View>
     )
   }
