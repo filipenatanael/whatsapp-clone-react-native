4893edff290e2c42cb02998c97b765d635dc6a7b
Filipe Natanael <filipenatanael@live.com>
2018-07-29 23:58:03 -0300

    Creating the text entry and submit button

--- source/components/Chat.js
+++ source/components/Chat.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, Image, TouchableHighlight } from 'react-native';
 
 export default class Chat extends Component {
   render(){
     return (
-      <View style={{ flex: 1, marginTop: 50, backgroundColor: '#eee4dc', padding: 10 }}>
+      <View style={{ flex: 1, marginTop: 10, backgroundColor: '#eee4dc', padding: 10 }}>
 
-      <View style={{ flex: 1, paddingBotton: 20  }}></View>
+      <View style={{ flex: 1, paddingBotton: 5 }}></View>
 
-      <View style={{ flexDirectio: 'row', height: 60 }}>
+      <View style={{ flexDirection: 'row', height: 60 }}>
         <TextInput
           underlineColorAndroid='transparent'
           style={{ flex: 4, backgroundColor: '#fff', fontSize: 15, borderRadius: 30 }}
         />
+
+        <TouchableHighlight onPress={() => false } underlayColor='#fff'>
+          <Image source={require('../images/ic_button_send_sms.png')} style={{ width: 60, height: 60, marginLeft: 5 }} />
+        </TouchableHighlight>
       </View>
 
       </View>
