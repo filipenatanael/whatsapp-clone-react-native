1b2a8a9186b598addec7e1d80d3044fe50b53a50
Filipe Natanael <filipenatanael@live.com>
2018-08-06 02:32:06 -0300

    Send message lexicon problem

--- source/reducers/AppReducer.js
+++ source/reducers/AppReducer.js
@@ -9,7 +9,6 @@ const INITIAL_STATE = {
 };
 
 export default (state = INITIAL_STATE, action) => {
-  console.log(action);
    switch (action.type) {
      case type.ADD_CONTACT:
         return { ...state, email_contact: action.payload }
