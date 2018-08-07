87736476bc251f5e28b61fb0a9636c3963389b98
Filipe Natanael <filipenatanael@live.com>
2018-08-07 00:09:52 -0300

    Create ListConversation reducer

--- source/reducers/index.js
+++ source/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux';
 import _AuthReducer from './AuthReducer';
 import _AppReducer from './AppReducer';
 import _ListContactsReducer from './ListContactsReducer';
+import _ListConversation from './ListConversation'
 
 export default combineReducers({
    AuthReducer: _AuthReducer,
    AppReducer: _AppReducer,
    ListContactsReducer: _ListContactsReducer,
+   ListConversation: _ListConversation
 });
