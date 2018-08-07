b3059aa1e2cff900ea3b8356a2ca1319d724abd9
Filipe Natanael <filipenatanael@live.com>
2018-08-07 00:49:52 -0300

    Viewing messages

--- source/components/Chat.js
+++ source/components/Chat.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { View, Text, TextInput, Image, TouchableHighlight } from 'react-native';
+import { View, Text, TextInput, Image, TouchableHighlight, ListView } from 'react-native';
 import { changeMessage, sendMessage, userConversationFetch } from '../actions/AppActions';
 
 class Chat extends Component {
 
   componentWillMount() {
-    this.props.userConversationFetch(this.props.contactEmail)
+    this.props.userConversationFetch(this.props.contactEmail);
+    this.createDataSource(this.props.conversation);
   }
-
 
   /* Component Context */
   _sendMessage() {
@@ -17,11 +17,36 @@ class Chat extends Component {
     this.props.sendMessage(message, contactName, contactEmail)
   }
 
+  createDataSource(conversation) {
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.dataSource = ds.cloneWithRows(conversation);
+    // this.dataSource is a variable on scope of this class
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.createDataSource(nextProps.conversation);
+  }
+
+  renderRow(text) {
+    return (
+      <View>
+        <Text>{text.message}</Text>
+        <Text>{text.type}</Text>
+      </View>
+    )
+  }
+
   render(){
     return (
       <View style={{ flex: 1, marginTop: 5, backgroundColor: '#eee4dc', padding: 10 }}>
 
-      <View style={{ flex: 1, paddingBottom: 20 }}></View>
+      <View style={{ flex: 1, paddingBottom: 20 }}>
+          <ListView
+            enableEmptySections
+            dataSource={this.dataSource}
+            renderRow={this.renderRow}
+          />
+      </View>
 
       <View style={{ flexDirection: 'row', height: 60 }}>
         <TextInput
