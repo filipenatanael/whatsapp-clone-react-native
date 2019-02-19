334e0eaec43b02ea4d241f419a02a89583c6818c
Filipe Natanael <filipenatanael@live.com>
2019-02-19 12:41:35 -0300

    refactor: organizing the actions types & reducers

--- source/actions/AuthActions.js
+++ source/actions/AuthActions.js
@@ -3,38 +3,45 @@ import { AsyncStorage } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import base64 from 'base-64';
 
-import * as type from '../resources/types';
+import {
+  ADD_NAME,
+  ADD_EMAIL,
+  ADD_PASSWORD,
+  SIGN_IN_LOADING,
+  AUTH_SUCCESS,
+  AUTH_FAILURE,
+  SIGN_UP_LOADING,
+  SUCCESS_REGISTER,
+  FAILURE_REGISTER,
+} from '../resources/types';
 
 /*
 ActionCreator to manipulate InputText on (SignUpScreen)
 */
 export const addName = (name) => {
   return {
-    type: type.ADD_NAME,
+    type: ADD_NAME,
     payload: name
   }
 }
 export const addEmail = (email) => {
   return {
-    type: type.ADD_EMAIL,
+    type: ADD_EMAIL,
     payload: email
   }
 }
 export const addPassword = (password) => {
   return {
-    type: type.ADD_PASSWORD,
+    type: ADD_PASSWORD,
     payload: password
   }
 }
-
-
 /*
 ActionCreator to signIn on application
 */
 export const SignIN = ({ email, password }) => {
   return dispatch => {
-    dispatch({ type: type.SIGN_IN_LOADING })
-
+    dispatch({ type: SIGN_IN_LOADING })
     firebase.auth().signInWithEmailAndPassword(email, password)
     .then(response => {
       const TOKEN_KEY = response.refreshToken;
@@ -44,21 +51,14 @@ export const SignIN = ({ email, password }) => {
     .catch(error => authUnsuccess(error, dispatch))
   }
 }
-
-const authSuccess = (dispatch) => {
-  dispatch({ type: type.AUTH_SUCCESS });
-  Actions.mainScreen();
-}
-const authUnsuccess = (error, dispatch) => {
-  dispatch({ type: type.AUTH_FAILURE, payload: error.code })
-}
-
 /*
 ActionCreator to create new user registration
 */
 export const registerUser = ({ name, email, password }) => {
   return dispatch => {
-    dispatch({ type: type.SIGN_UP_LOADING })
+    dispatch({ type:
+      SIGN_UP_LOADING
+    })
 
     firebase.auth().createUserWithEmailAndPassword(email, password)
     .then(response => {
@@ -71,11 +71,28 @@ export const registerUser = ({ name, email, password }) => {
   }
 }
 
+const authSuccess = (dispatch) => {
+  dispatch({
+    type: AUTH_SUCCESS
+  });
+  Actions.mainScreen();
+}
+
+const authUnsuccess = (error, dispatch) => {
+  dispatch({
+    type: AUTH_FAILURE,
+    payload: error.code
+  })
+}
+
 const registerSuccess = (dispatch) => {
-  dispatch({ type: type.SUCCESS_REGISTER });
+  dispatch({ type: SUCCESS_REGISTER });
   Actions.welcomeScreen();
 }
 
 const registerUnsuccess = (error, dispatch) => {
-  dispatch({ type: type.FAILURE_REGISTER, payload: error.code })
+  dispatch({
+    type: FAILURE_REGISTER,
+    payload: error.code
+  })
 }
--- source/reducers/ListContactsReducer.js
+++ source/reducers/ListContactsReducer.js
@@ -1,11 +1,12 @@
-import * as type from './../resources/types';
-
 const INITIAL_STATE = {}
 
+import {
+  CONTACTS_LIST
+} from './../resources/types';
+
 export default (state = INITIAL_STATE, action) => {
-  //console.log('Testing: ', action.payload);
   switch(action.type) {
-    case type.CONTACTS_LIST:
+    case CONTACTS_LIST:
       return action.payload
     default:
       return state;
